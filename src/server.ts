53e164fc7cb3692ecc76b2bd258120a5a6753604
Drew Kimberly <andrew.e.kimberly@gmail.com>
2020-02-10 07:48:24 -0500

    Implement /filteredimage REST endpoint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, { Express } from 'express';
+import cors, { CorsOptions } from 'cors';
 import bodyParser from 'body-parser';
-import { filterImageFromURL, deleteLocalFiles } from './util/util';
+import { IndexRouter } from './controllers/v0/index.router';
 
 (async () => {
   // Init the Express application
-  const app = express();
+  const app: Express = express();
 
   // Set the network port
   const port = process.env.PORT || 8082;
@@ -12,29 +13,29 @@ import { filterImageFromURL, deleteLocalFiles } from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  // Setup CORS middleware.
+  const originWhitelist: string[] = ['http://localhost:8102'];
+  const corsOptions: CorsOptions = {
+    allowedHeaders: [
+      'Origin',
+      'X-Requested-With',
+      'Content-Type',
+      'Accept',
+      'Authorization'
+    ],
+    origin: (
+      origin: string,
+      callback: (err: Error | null, allow?: boolean) => void
+    ) =>
+      originWhitelist.includes(origin)
+        ? callback(null, true)
+        : callback(null, false)
+  };
+  app.use(cors(corsOptions));
+  app.options('*', cors());
 
-  /**************************************************************************** */
-
-  //! END @TODO1
-
-  // Root Endpoint
-  // Displays a simple message to the user
-  app.get('/', async (req, res) => {
-    res.send('try GET /filteredimage?image_url={{}}');
-  });
+  // Route all API traffic to our index router.
+  app.use('/api/v0/', IndexRouter);
 
   // Start the Server
   app.listen(port, () => {
