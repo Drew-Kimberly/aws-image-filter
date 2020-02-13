7b40ed2e55540aafc50277b7711fc8ff0e962f7e
Drew Kimberly <andrew.e.kimberly@gmail.com>
2020-02-13 09:29:22 -0500

    Update API key header name b/c EB is weird about underscores

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -5,11 +5,11 @@ export const requireAuth = (
   res: Response,
   next: NextFunction
 ) => {
-  if (!req.headers || !req.headers.api_key) {
+  if (!req.headers || !req.headers.apikey) {
     return res.status(401).send({ message: 'Unauthorized' });
   }
 
-  const apiKey: string = req.headers.api_key as string;
+  const apiKey: string = req.headers.apikey as string;
   if (apiKey.length === 0 || apiKey !== process.env.IMAGE_FILTER_API_KEY) {
     return res.status(403).send({ message: 'Access Denied' });
   }
